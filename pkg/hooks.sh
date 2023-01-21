f60c1ff0309273629dae4c10c593c79a77d92dd2
Markus Felten <Markus.Felten@gmx.de>
2023-01-21 14:57:42 +0100

    fix: use install.dir

--- pkg/hooks.sh
+++ pkg/hooks.sh
@@ -1,16 +1,16 @@
 
 post_install() {
-	(cd {{installdir}} && find -type f ! -name "*.gz" ! -name "*.png" -print0|xargs -0 -r gzip -k -9)
+	(cd {{install.dir}} && find -type f ! -name "*.gz" ! -name "*.png" -print0|xargs -0 -r gzip -k -9)
 }
 
 pre_upgrade() {
-	(cd {{installdir}} && find -type f -name "*.gz" -print0|xargs -0 -r rm)
+	(cd {{install.dir}} && find -type f -name "*.gz" -print0|xargs -0 -r rm)
 }
 
 post_upgrade() {
-	(cd {{installdir}} && find -type f ! -name "*.gz" -print0|xargs -0 -r gzip -k -9)
+	(cd {{install.dir}} && find -type f ! -name "*.gz" -print0|xargs -0 -r gzip -k -9)
 }
 
 pre_remove() {
-	(cd {{installdir}} && find -type f -name "*.gz" -print0|xargs -0 -r rm)
+	(cd {{install.dir}} && find -type f -name "*.gz" -print0|xargs -0 -r rm)
 }
