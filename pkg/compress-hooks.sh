e19478938d5edd60def0918987bd6e57893482f1
Markus Felten <Markus.Felten@gmx.de>
2026-02-05 16:03:25 +0000

    fix: use _ for property names
    BREAKING CHANGE: all property names with _ instead of '.'

--- pkg/compress-hooks.sh
+++ pkg/compress-hooks.sh
@@ -1,16 +1,16 @@
 
 post_install() {
-	(cd {{install.dir}} && find -size +500 -type f ! -name "*.gz" ! -name "*.br" ! -name "*.jpg" ! -name "*.png"  ! -name "*.webp" -print0|xargs -0 -r brotli -f)
+	(cd {{install_dir}} && find -size +500 -type f ! -name "*.gz" ! -name "*.br" ! -name "*.jpg" ! -name "*.png"  ! -name "*.webp" -print0|xargs -0 -r brotli -f)
 }
 
 pre_upgrade() {
-	(cd {{install.dir}} && find -type f -name "*.br" -o -name "*.gz" -print0|xargs -0 -r rm)
+	(cd {{install_dir}} && find -type f -name "*.br" -o -name "*.gz" -print0|xargs -0 -r rm)
 }
 
 post_upgrade() {
-	(cd {{install.dir}} && find -size +500 -type f ! -name "*.gz" ! -name "*.br" ! -name "*.jpg" ! -name "*.png"  ! -name "*.webp" -print0|xargs -0 -r brotli -f)
+	(cd {{install_dir}} && find -size +500 -type f ! -name "*.gz" ! -name "*.br" ! -name "*.jpg" ! -name "*.png"  ! -name "*.webp" -print0|xargs -0 -r brotli -f)
 }
 
 pre_remove() {
-	(cd {{install.dir}} && find -type f -name "*.br" -o -name "*.gz" -print0|xargs -0 -r rm)
+	(cd {{install_dir}} && find -type f -name "*.br" -o -name "*.gz" -print0|xargs -0 -r rm)
 }
